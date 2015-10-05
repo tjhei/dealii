1fa669cc081413f5e2cbbd2b8e7db90acdf77102
Timo Heister <timo.heister@gmail.com>
2015-10-05 12:48:54 -0400

    appveyor

--- tests/run_testsuite.cmake
+++ tests/run_testsuite.cmake
@@ -472,7 +472,7 @@ IF("${_res}" STREQUAL "0")
   # Only run the build stage if configure was successful:
 
   MESSAGE("-- Running CTEST_BUILD()")
-  CTEST_BUILD(TARGET ${MAKEOPTS} NUMBER_ERRORS _res)
+  CTEST_BUILD(${MAKEOPTS} NUMBER_ERRORS _res)
 
   IF("${_res}" STREQUAL "0")
     # Only run tests if the build was successful:
