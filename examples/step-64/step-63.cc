ea40fb2f07f4c5175a8139e2fbeba1ee81f68259
Timo Heister <timo.heister@gmail.com>
2019-04-09 10:36:12 -0600

    edit files

--- examples/step-64/step-63.cc
+++ examples/step-64/step-63.cc
@@ -1,6 +1,6 @@
 /* ---------------------------------------------------------------------
  *
- * Copyright (C) 2018 - 2019 by the deal.II authors
+ * Copyright (C) 2019 by the deal.II authors
  *
  * This file is part of the deal.II library.
  *
@@ -13,7 +13,7 @@
  *
  * ---------------------------------------------------------------------
  *
- * Authors: Thomas Clevenger, Clemson University
+ * Authors: Fabian Castelli, Karlsruhe Institute of Technology (KIT)
  *          Timo Heister, University of Utah
  */
 
